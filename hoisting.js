42ae66c9f32f98b1344f61946642df4d4439eef0
zakir matin <zakir.matin@gmail.com>
2020-02-06 17:34:00 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,10 @@
+ var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 var thisIsCrazy = "hey!!";
@@ -19,9 +19,9 @@ function sayMyName() {
   var name = "Cricky";
 
   sayMy();
-   var name = "Kristin";
+
   function sayMy() {
+    var name = "Kristin";
     console.log(name);
   }
 }
- sayMy();
