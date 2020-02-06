c7af850f9e28185ba3ed9d8772305a5be8ec8e4f
zakir matin <zakir.matin@gmail.com>
2020-02-06 17:25:01 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,10 +18,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-
+  sayMy();
    var name = "Kristin";
   function sayMy() {
     console.log(name);
+    var name = "Kristin";
   }
 }
  sayMy();
